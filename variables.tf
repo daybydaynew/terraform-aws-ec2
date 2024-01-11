7b44fc8b5490cdfe1c20b67e5a424a526abf222f
daybydaynew <106713248+daybydaynew@users.noreply.github.com>
2024-01-11 10:07:47 +0700

    Update variables.tf
    Signed-off-by: daybydaynew <106713248+daybydaynew@users.noreply.github.com>

--- variables.tf
+++ variables.tf
@@ -5,32 +5,25 @@ variable "subnet_id" {
 
 variable "region" {
   type = string
-  
 }
 
 variable "instance_type" {
   type = string
-  
 }
 
 variable "instance_name" {
   type = string
   default = "helloworld"
-  
 }
+
 variable "ssh_key" {
   type = string
-  
 }
 
-}
 variable "access_key" {
   type = string
-  
 }
 
-}
 variable "secret_key" {
   type = string
-  
 }
